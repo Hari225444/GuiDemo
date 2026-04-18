235ee21d2bae97665cac8c557ddc8b42aac507bf
haritha <muthyalaharitha@gmail.com>
2026-04-18 14:46:26 +0530

    India commit

--- src/basic/Methods.java
+++ src/basic/Methods.java
@@ -4,7 +4,7 @@ public class Methods {
 	
 public String ValidateHeader()
 	{
-		System.out.println("Header links validated test");
+		System.out.println("Header links validated test my git is working good");
 		return "Pass";
 	}
 
